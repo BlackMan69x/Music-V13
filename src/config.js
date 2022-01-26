f48cb1f92999fef202bb49e161a979a7b83bcecb
Black Man <80176569+zeko628@users.noreply.github.com>
2022-01-26 16:13:39 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
     ownerID: "802491275445010443",
     SpotifyID: "bf5ee2a72bae40ffadc71a47280e5ff9",
     SpotifySecret: "053469ffeb3844079fab734ebf3090c2",
-    mongourl: "mongodb+srv://monty:rama2211@cluster0.xk22y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongourl: "mongodb+srv://carloganjos12:carloganjos12@cluster0.vj8lc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     embedColor: "RANDOM",
     logs: "933777715247611924",
 
